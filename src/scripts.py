9c4797d66c225ab17e8cbd2d977e700bf8746b7a
Max de Visser <m.madv96@gmail.com>
2022-12-09 15:39:16 +0100

    made progess on gap-statistic. Made function for calculating cluster intravariablility

--- src/scripts.py
+++ src/scripts.py
@@ -9,10 +9,28 @@ def calculate_centroid(cluster_df: pd.DataFrame) -> list:
     """Return the centroid of a cluster as a list"""
     return cluster_df.describe().loc['mean']
 
-def intra_cluster_variance(cluster: pd.DataFrame) -> float:
-    centroid = calculate_centroid(cluster)
-    [dist(instance, centroid) for instance in cluster.values.tolist()]
-    return np.sum()
+def split_in_clusters(cluster_df: pd.DataFrame) -> list:
+    cluster_list = [] # a list to hold each cluster as a DataFrame
+    for i in range(len(cluster_df.cluster.unique())):
+        cluster_list.append(cluster_df.loc[cluster_df['cluster'] == i])
+    return cluster_list
+
+def intra_cluster_variance(cluster_df: pd.DataFrame) -> float:  
+    cluster_list = split_in_clusters(cluster_df)
+    
+    # calculate centroid in each cluster
+    cluster_centroids = [calculate_centroid(cluster) for cluster in cluster_list]
+    
+    # calculate intra variance in each cluster and store in list
+    cluster_intra_variance = []
+    for (cluster, centroid) in zip(cluster_list, cluster_centroids):
+        cluster_intra_variance.append(sum([dist(instance, centroid) for instance in cluster.values.tolist()]))
+    
+    return cluster_list, cluster_intra_variance
+    # A 2D list of distances between each instance and its centroid in each cluster
+    #cluster_centroid_distances = [[dist(instance, centroid) for instance in cluster.values.tolist()] for cluster in cluster_list]
+    
+    #return cluster_centroid_distances # np.sum(intra_variances)
 
 def WCSS(cluster: list):
     """Calculate the Within-Cluster-Sum of Squared-Errors (WSS)
